c98f34618140c0a0076bb21e1964bd2d859a88c0
Jessa <jessacabigas@gmail.com>
2017-04-27 16:41:32 +0800

    Creating pickle objects,

--- Preprocessing/remove_punctuations.py
+++ Preprocessing/remove_punctuations.py
@@ -1,19 +1,39 @@
 import string
-import re
+import csv
+import regex as re
 
-def removePunctuation(tweet):
-    s = re.sub(r'[^\w\s]','',tweet)
-    return tweet
+def removePunctuations(tweet):
 
-def removePunctuation2(tweet):
-    out = tweet.translate(None, r"!\"#$%&'()*+,./:;<=>?-[\]^_`{|}~")
-    # if out.startswith(" "):
-        # out = out[1:]
-    return out
+     out = tweet.translate(None, r"!\"#$%&'()*+,./:;<=>?-[\]^_`{|}~")
+    #  test = filter(lambda ch: ch not in "!#""$%&'()*+,./:;<=>?-[\]^_`{|}~", tweet)
+    #  out = ""
+    #  punct = "!\"#$%&'()*+,./:;<=>?-[\]^_`{|}~"
+    #  for char in tweet:
+    #      if char not in punct:
+    #          out = out + char
+    #  return re.sub(ur"[^\P{P}@]+", "", tweet)
+     return out
 
-with open("../Dataset/final-data.csv",'r') as f:
-    for line in f.readlines():
+if __name__ == "__main__":
+
+    tweets = []
+
+    with open("../Dataset/final-data.csv",'r') as r, open("../Dataset/tweets-removed-punctuations-test.csv", 'a') as w:
+        reader = csv.DictReader(r)
+
+        fieldnames = ['tweet','label']
+        writer = csv.DictWriter(w, fieldnames=fieldnames)
+
+        for row in reader:
+            tweets.append(row)
+
+
+        writer.writeheader()
+
+        for line in r.readlines():
             tweet=line[:-2]
             label= line[-2:]
-            with open("../Dataset/tweets-removed-punctuations-test.csv", 'a') as r:
-                r.write(removePunctuation2(tweet)+","+label)
+
+        for t in tweets:
+            ar = t['tweet'].lower()
+            writer.writerow({'tweet': removePunctuations(ar), 'label':t['label']})
